0694c8ec77cfe94e88ff39033b48c862a902caf8
Kenni <kenni.bawden@gmail.com>
2014-11-19 14:29:41 +1100

    Changed testing to output to stdout/stderr
    This should help Travis CI give us better feedback

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-from subprocess import call
+import sys
 from os import listdir
 from os.path import isfile, join
+from tornado.testing import main
 
 # Get files in test directory
 TEST_PATH = 'tests'
@@ -8,4 +9,5 @@ test_files = [join(TEST_PATH, f) for f in listdir(TEST_PATH) if isfile(join(TEST
 
 # Run each of the tests
 for test_file in test_files:
-	call(['python3', '-m', 'tornado.testing', test_file])
+	sys.argv = [sys.argv[0]] + [test_file]
+	main()
