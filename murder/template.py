ae30b6ca3c7d879b555e97a5d7d7433931c3395c
Kenni <kenni.bawden@gmail.com>
2014-12-07 13:43:01 +1100

    Added template function to the template namespace
    This can be used to include templates with separate name spaces, e.g.,
    ``` {% raw template('link.html', url=game) %} ```

--- murder/template.py
+++ murder/template.py
@@ -1,5 +1,25 @@
-import tornado.template
-templater = tornado.template.Loader('static/html')
+import os
+from tornado.template import Loader, Template
+
+class MurderTemplate(Template):
+	def generate(self, **kwargs):
+		kwargs.update({
+			'template': template_loader,
+		})
+		return super(MurderTemplate, self).generate(**kwargs)
+
+class TemplateLoader(Loader):
+	def _create_template(self, name):
+		path = os.path.join(self.root, name)
+		f = open(path, "rb")
+		template = MurderTemplate(f.read(), name=name, loader=self)
+		f.close()
+		return template
+
+templater = TemplateLoader('static/html')
+
+def template_loader(template, **kwargs):
+	return templater.load(template).generate(**kwargs)
 
 def page_template() -> str:
 	return templater.load('page.html')
