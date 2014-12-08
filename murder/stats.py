e1067563d66747ddaf8af5dbfa7e4ae099583f1e
Kenni <kenni.bawden@gmail.com>
2014-12-08 14:47:24 +1100

    Added most wanted to the stats

--- murder/stats.py
+++ murder/stats.py
@@ -1,12 +1,20 @@
+from collections import Counter
+
 from .template import templater, inside_page
 from .player import Player
 from .murder import Murder
 
-def stats_template(game_id, players, murders) -> str:
-	stats = templater.load('stats.html').generate(game_id=game_id, murders=murders, players=players)
+def stats_template(game_id, players, murders, **kwargs) -> str:
+	stats = templater.load('stats.html').generate(game_id=game_id, murders=murders, players=players, **kwargs)
 	return inside_page(stats, game_id=game_id)
 
 def stats(response, game_id=None):
 	players = list(Player.iter(game=game_id))
 	murders = list(Murder.iter(game=game_id))
-	response.write(stats_template(game_id, players, murders))
+
+	murder_counts = Counter(murder.murderer for murder in murders)
+	murderer_id, count = max(murder_counts.items())
+	most_wanted = Player.find(game=game_id, id=murderer_id)
+	most_wanted.murders = count
+
+	response.write(stats_template(game_id, players, murders, most_wanted=most_wanted))
